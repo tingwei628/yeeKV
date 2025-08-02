62e366309c187f3e26271feb95cc817b50f0d56f
tingwei628 <sbvq6p199xe@gmail.com>
2025-08-02 20:49:55 +0800

    fix

--- app/main.go
+++ app/main.go
@@ -597,6 +597,8 @@ func handleConnection(conn net.Conn) {
 			isReadFirstByte = true
 			commands = make([]string, 0, command_count)
 
+			fmt.Printf("commands %v\n", commands)
+
 		} else if strings.HasPrefix(text, "$") {
 			readBulkCommand = true
 			continue
